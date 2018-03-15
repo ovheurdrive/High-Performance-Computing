7a7bce05a591935539d3b1277ba35418d1acdd8d
Stanislas Plessia <stanislas.plessia@streem.com.au>
2018-03-15 18:46:05 +0100

    Add data processing for different proc

--- Project/src/main.c
+++ Project/src/main.c
@@ -8,20 +8,39 @@
 
 int main( int argc, char* argv[]) {
   int ret = 0;
-  unsigned int dim_row = 2;
-  unsigned int dim_col = 2;
-  if( argc > 2) {
-    dim_row = atoi(argv[1]);
-    dim_col = atoi(argv[2]);
-  }
 
   int rank, size;
   MPI_Init(&argc, &argv);               // starts MPI
   MPI_Comm_rank(MPI_COMM_WORLD, &rank); // get current process id
   MPI_Comm_size(MPI_COMM_WORLD, &size); // get number of processes
 
+  int root = size - 1;
   Matrix mtx = { 0, 0, NULL };
   Vector vect = { 0, NULL };
+
+  char filename[100];
+  snprintf(filename, sizeof(filename), "data/matrix_%d.txt", rank + 1 );
+
+  if( (ret = read_matrix_from_file(&mtx, filename)) != 0) {
+    fprintf(stderr, "Error when constructing matrix from file in proc %d\n", rank);
+    MPI_Abort(MPI_COMM_WORLD, ret);
+  }
+
+  char message[100];
+  snprintf(message, sizeof(message), "Matrix from proc %d :", rank);
+  display_matrix(&mtx, message);
+
+
+  snprintf(filename, sizeof(filename), "data/vector_%d.txt", rank + 1 );
+
+  if( (ret = read_vector_from_file(&vect, filename)) != 0) {
+    fprintf(stderr, "Error when constructing vector from file in proc %d\n", rank);
+    MPI_Abort(MPI_COMM_WORLD, ret);
+  }
+
+  snprintf(message, sizeof(message), "Vector from proc %d :", rank);
+  display_vector(&vect, message);
+
 
   MPI_Finalize();
   return ret;
