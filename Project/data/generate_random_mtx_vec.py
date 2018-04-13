3a7eb45784bb092f6561b5352d9cc0aab83e0f08
Stanislas Plessia <stanislas.plessia@streem.com.au>
2018-04-13 16:57:17 +0200

    Update matrix script to create hollow matrices

--- Project/data/generate_random_mtx_vec.py
+++ Project/data/generate_random_mtx_vec.py
@@ -10,13 +10,15 @@ with open("matrix_t.txt", "w") as f1:
   for i in range(size):
     for j in range(size):
       if i == j:
-        f1.write(str(random.randint(25,50)) + ' ')
+        f1.write(str(random.randint(10,15)) + ' ')
+      elif abs(i-j) < 4:
+        f1.write(str(random.randint(-2,2)) + ' ')
       else:
-        f1.write(str(random.randint(-5,5)) + ' ')
+        f1.write('0' + ' ')
     f1.write('\n')
 
 with open("vector_t.txt", "w") as f2:
   for i in range(size):
-    f2.write(str(random.randint(0,2)))
+    f2.write(str(random.randint(0,6)))
     f2.write("\n")
 
